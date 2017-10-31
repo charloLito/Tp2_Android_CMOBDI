6086a228a8f976f65f4d7696556e2ca1e5a347e8
charloLito <1362270@cstj.qc.ca>
2017-10-31 10:47:09 -0400

    Début classe Livre

---
+++ app/src/main/java/ca/qc/android/cstj/bibliocm/models/Categorie.kt
@@ -0,0 +1,12 @@
+package ca.qc.android.cstj.bibliocm.models
+
+import com.github.kittinunf.fuel.android.core.Json
+import org.json.JSONObject
+/**
+ * Created by Administrateur on 2017-10-31.
+ */
+class Livres(jsonObject: Json){
+    var titre:String=jsonObject.obj().getString("titre")
+    var prix:Double=jsonObject.obj().getDouble("prix")
+
+}
