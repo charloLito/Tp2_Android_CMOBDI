ad35f52326272cafbf2e4aca20ec03187739663f
Miguel Dupuis <1547201@cstj.qc.ca>
2017-10-31 11:49:11 -0400

    Ajout class Catégorie

--- app/src/main/java/ca/qc/android/cstj/bibliocm/models/Categorie.kt
+++ app/src/main/java/ca/qc/android/cstj/bibliocm/models/Categorie.kt
@@ -1,13 +1,11 @@
 package ca.qc.android.cstj.bibliocm.models
 
 import com.github.kittinunf.fuel.android.core.Json
-import org.json.JSONObject
-/**
- * Created by Administrateur on 2017-10-31.
- */
-class Livres(jsonObject: Json){
-    var titre:String=jsonObject.obj().getString("titre")
-    var prix:Double=jsonObject.obj().getDouble("prix")
 
+
+
+class Categorie(jsonObject: Json){
+
+    var categorie:String=jsonObject.obj().getString("categorie")
 
 }
